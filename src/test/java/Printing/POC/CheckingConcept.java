b2031c5ceb62dcf7c45b83d0d06a057bc7471fd1
IGS0502 <IGS0502@172.31.99.69>
2022-10-14 09:38:06 +0530

    init

---
+++ src/test/java/Printing/POC/CheckingConcept.java
@@ -0,0 +1,18 @@
+package Printing.POC;
+
+import org.testng.annotations.Test;
+
+public class CheckingConcept {
+
+	
+	@Test
+	public void execute() {
+		StringBuilder sb = new StringBuilder(4);
+		sb.append("shashi kumar");
+
+		System.out.println(sb);
+		System.out.println(sb.length());
+		System.out.println(sb.capacity());
+		System.out.println("Git 2nd Push");
+	}
+}
