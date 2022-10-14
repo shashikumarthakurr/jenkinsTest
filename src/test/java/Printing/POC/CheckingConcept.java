b284ae01b1807a8ff2e469aea8bf73881fc1fee4
IGS0502 <IGS0502@172.31.99.69>
2022-10-14 10:03:13 +0530

    dre

--- src/test/java/Printing/POC/CheckingConcept.java
+++ src/test/java/Printing/POC/CheckingConcept.java
@@ -14,6 +14,6 @@ public class CheckingConcept {
 		System.out.println(sb.length());
 		System.out.println(sb.capacity());
 		System.out.println("Git 2nd Push");
-		System.out.println("edit");
+		
 	}
 }
